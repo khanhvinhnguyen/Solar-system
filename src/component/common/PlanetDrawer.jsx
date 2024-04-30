d0b5720648916b0b2af0c9f67c15dfb282827b62
khanhvinhnguyen <khanhvinh.nguyen.0628@gmail.com>
2024-05-01 01:28:24 +0700

    change: UI planet data

--- src/component/common/PlanetDrawer.jsx
+++ src/component/common/PlanetDrawer.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import { Checkbox, Divider, Button } from "antd";
 import { CloseOutlined } from "@ant-design/icons";
 import intl from "react-intl-universal";
-import * as THREE from "three";
 
 import { SettingContext } from "../context/SettingContext";
 import { usePlanet } from "../context/PlanetSelectContext";
@@ -18,7 +17,7 @@ const PlanetDrawer = () => {
   };
 
   const planetDataLang = language == "en" ? planetData_EN : planetData_VN;
-  const planetInfo = selectedPlanet ? planetDataLang[selectedPlanet] : null;
+  const planetInfo = planetDataLang[selectedPlanet];
 
   const handleCloseDrawer = () => {
     selectPlanet(null);
@@ -34,59 +33,68 @@ const PlanetDrawer = () => {
         </Button>
       </div>
 
+      <Divider style={{ backgroundColor: "gray" }} />
+
       {/* Info  */}
       <ul>
-        <li>
-          <strong>{intl.get(`title.introduction`)}</strong>{" "}
-          <br></br>
-          {planetInfo.introduction}
+        {/* Introduction */}
+        <li className="planetDrawer_subtitle">
+          {intl.get(`title.introduction`)}
         </li>
-        <br></br>
-        <li>
-          <strong>{intl.get(`title.encyclopedia`)}</strong>
-        </li>
+        <p>{planetInfo.introduction}</p>
 
-          {"- "}{intl.get(`title.equatorialDiameter`)}:{" "}
-          {planetInfo.equatorialDiameter}
-          <br></br>
-          {"- "}{intl.get(`title.mass`)}:{" "}
-          {planetInfo.mass}
-          <br></br>
-          {"- "}{intl.get(`title.distance`)}:{" "}
-          {planetInfo.distance}
-          <br></br>
-          {"- "}{intl.get(`title.rotationPeriod`)}:{" "}
-          {planetInfo.rotationPeriod}
-          <br></br>
-          {"- "}{intl.get(`title.orbitPeriod`)}:{" "}
-          {planetInfo.orbitPeriod}
-          <br></br>
-          {"- "}{intl.get(`title.surfaceGravity`)}:{" "}
-          {planetInfo.surfaceGravity}
-          <br></br>
-          {"- "}{intl.get(`title.surfaceTemperature`)}:{" "}
-          {planetInfo.surfaceTemperature}
-          <br></br>
-        <br></br>
-
-        <li>
-          <strong>{intl.get(`title.anotherInformation`)}</strong>
+        {/* Encyclopedia */}
+        <li className="planetDrawer_subtitle">
+          {intl.get(`title.encyclopedia`)}
         </li>
-          {planetInfo.information1}
-          <br></br>
-          {planetInfo.information2}
-          <br></br>
-          {planetInfo.information3}
-          <br></br>
-          {planetInfo.information4}
+        <ul>
+          <li>
+            {intl.get(`title.equatorialDiameter`)}:{" "}
+            {planetInfo.equatorialDiameter}
+          </li>
+          <li>
+            {intl.get(`title.mass`)}: {planetInfo.mass}
+          </li>
+          <li>
+            {intl.get(`title.distance`)}: {planetInfo.distance}
+          </li>
+          <li>
+            {intl.get(`title.rotationPeriod`)}: {planetInfo.rotationPeriod}
+          </li>
+          <li>
+            {intl.get(`title.orbitPeriod`)}: {planetInfo.orbitPeriod}
+          </li>
+          <li>
+            {intl.get(`title.surfaceGravity`)}: {planetInfo.surfaceGravity}
+          </li>
+          <li>
+            {intl.get(`title.surfaceTemperature`)}:{" "}
+            {planetInfo.surfaceTemperature}
+          </li>
+        </ul>
+
+        {/* Another Information */}
+        <li className="planetDrawer_subtitle">
+          {intl.get(`title.anotherInformation`)}
+        </li>
+        <ul>
+          {Object.keys(planetInfo.information).map((key) => (
+            <li key={key}>
+              <strong>
+                <i>{intl.get(`subtitle.${key}`)}: </i>
+              </strong>
+              {planetInfo.information[key]}
+            </li>
+          ))}
+        </ul>
       </ul>
 
       <Divider style={{ backgroundColor: "gray" }} />
 
       {/* Option structure*/}
-      <Checkbox onChange={handleStructure}>
+      {/* <Checkbox onChange={handleStructure}>
         {intl.get(`general.structure`)}
-      </Checkbox>
+      </Checkbox> */}
     </div>
   ) : null;
 };
--- src/locales/vi_VN.js
+++ src/locales/vi_VN.js
@@ -1,9 +1,9 @@
 export default {
-  'general.settings': 'Settings',
-  'general.structure': 'Structure',
-  'settings.orbitLine': 'Orbit Line',
+  'general.settings': 'Cài đặt',
+  'general.structure': 'Cấu trúc',
+  'settings.orbitLine': 'Đường quỹ đạo',
   'language.english': 'English',
-  'language.vietnamese': 'Vietnamese',
+  'language.vietnamese': 'Tiếng Việt',
 
   // 1. Planet Name
   'planetName.Sun': 'Mặt Trời',
@@ -18,8 +18,9 @@ export default {
   'planetName.Neptune': 'Sao Hải Vương',
 
   // 2. Title detail
-  'title.introduction': 'GIỚI THIỆU',
-  'title.encyclopedia': 'BÁCH KHOA TOÀN THƯ',
+  'title.introduction': 'giới thiệu',
+  'title.encyclopedia': 'bách khoa toàn thư',
+  'title.anotherInformation': 'thông tin khác',
   'title.equatorialDiameter': 'Đường kính xích đạo',
   'title.mass': 'Khối lượng',
   'title.distance': 'Khoảng cách',
@@ -27,6 +28,18 @@ export default {
   'title.orbitPeriod': 'Chu kỳ quỹ đạo',
   'title.surfaceGravity': 'Trọng lực bề mặt',
   'title.surfaceTemperature': 'Nhiệt độ bề mặt',
-  'title.anotherInformation': 'THÔNG TIN KHÁC',
 
+  'subtitle.composition': "Thành phần",
+  'subtitle.distance': "Khoảng cách",
+  'subtitle.galaxy': "Thiên hà",
+  'subtitle.observation': "Quan sát",
+  'subtitle.atmosphere': "Khí quyển",
+  'subtitle.surface': "Bề mặt",
+  'subtitle.axialTilt': "Góc nghiêng trục",
+  'subtitle.magField': "Từ trường",
+  'subtitle.mass': "Khối lượng",
+  'subtitle.water': "Nước",
+  'subtitle.moons': "Mặt trăng",
+  'subtitle.starLike': "Star-Like",
+  'subtitle.ring': "Vành đai",
 };

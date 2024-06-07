6776804c2a415aa504268ca178946bae5aa6fde0
khanhvinhnguyen <khanhvinh.nguyen.0628@gmail.com>
2024-06-07 13:28:15 +0700

    add: loading background

--- src/component/common/LoadingOverlay.jsx
+++ src/component/common/LoadingOverlay.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { useProgress } from "@react-three/drei";
-import { Spin } from "antd";
+import { Spin, Flex } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
 
 const LoadingOverlay = () => {
   const { progress } = useProgress();
 
   return (
     <div className="loading-overlay">
-      <div align="center" gap="middle">
-        <Spin size="large" />
-        {/* <p>{progress.toFixed(2)}% loaded</p> */}
+      <div className="loading-border" align="center" gap="middle">
+        <Flex gap="small">
+          {/* <Spin tip="Loading" size="large" /> */}
+          <Spin
+            indicator={
+              <LoadingOutlined
+                style={{
+                  fontSize: 50,
+                }}
+                spin
+              />
+            }
+          />
+        </Flex>
       </div>
     </div>
   );

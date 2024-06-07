e5fbb490ecae41670d2d65500860a7ffbb01538b
khanhvinhnguyen <khanhvinh.nguyen.0628@gmail.com>
2024-06-07 11:41:56 +0700

    Add _headers file to ensure correct MIME type for JS files

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,16 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/CS105-solar-system/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  base: '/',
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
   }
 })
